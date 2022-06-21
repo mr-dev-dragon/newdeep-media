3e2c9bf0e23be82f1c844561ec01a1f824c253bc
Ahmed Aoulad Amar <71600486+Ahmed-dev-dragon@users.noreply.github.com>
2022-06-21 13:23:20 -0700

    add

---
+++ backend/schemas/save.js
@@ -0,0 +1,19 @@
+export default {
+    name: 'save',
+    title: 'Save',
+    type: 'document',
+    fields: [
+      {
+        name: 'postedBy',
+        title: 'PostedBy',
+        type: 'postedBy',
+      },
+      {
+        name: 'userId',
+        title: 'UserId',
+        type: 'string',
+      },
+    ],
+  };
+  
+  
