1c69251a35298ceb1aa5cdaf4543ad458f555202
Ahmed Aoulad Amar <71600486+Ahmed-dev-dragon@users.noreply.github.com>
2022-06-21 13:10:36 -0700

    add

---
+++ backend/schemas/pin.js
@@ -0,0 +1,47 @@
+export default {
+    name : ' pin ' ,
+    title : ' Pin ' ,
+    type : ' document ' ,
+    fields : 
+            [
+                {
+                    name : ' title ' ,
+                    title : ' Title ' ,
+                    type : ' string ' ,
+                },
+                {
+                    name : ' about ' ,
+                    title : ' About ' ,
+                    type : ' string ' ,
+                },
+                {
+                    name: 'destination',
+                    title: 'Destination',
+                    type: 'url' ,
+                },
+                {
+                    name: 'category',
+                    title: 'Category',
+                    type: 'string' ,
+                },
+                {
+                    name: 'image',
+                    title: 'Image',
+                    type: 'image' ,
+                    options:{
+                        hotspot : true
+                    }
+                },                {
+                    name: 'destination',
+                    title: 'Destination',
+                    type: 'url' ,
+                },                {
+                    name: 'destination',
+                    title: 'Destination',
+                    type: 'url' ,
+                },
+            ]
+        
+        
+        }
+  
