6ae58d3cdb213b9d82a1615f68538a8db9efa92a
Ahmed Aoulad Amar <71600486+Ahmed-dev-dragon@users.noreply.github.com>
2022-06-21 18:03:41 -0700

    add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 
-import { Login } from './components';
-import Home from './container/Home';
-
+// import { Login } from './components';
+// import Home from './container/Home';
+import React from "react";
 const App = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  useEffect(() => {
-    const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+  // useEffect(() => {
+  //   const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
 
-    if (!User) navigate('/login');
-  }, []);
+  //   if (!User) navigate('/login');
+  // }, []);
 
   return (
-    <Routes>
-      <Route path="login" element={<Login />} />
-      <Route path="/*" element={<Home />} />
-    </Routes>
-  );
-};
+    <h1 className="text-3xl font-bold underline">
+      hello world
+    </h1>
+    // <Routes>
+    //   <div > hiii ther</div>
+    //   {/* <Route path="login" element={<Login />} />
+    //   <Route path="/*" element={<Home />} /> */}
+    // </Routes>
+  )
+}
 
 export default App;
